69c3e44a51664118e577cb979b23b35961cc11cb
SABARINATH-23 <sabarinath0211@gmail.com>
2025-04-25 00:11:30 +0530

    multiple task added

--- Project/Javascript/Create.js
+++ Project/Javascript/Create.js
@@ -1,25 +1,14 @@
 let title = document.getElementById("title");
 let content = document.getElementById("content");
 let create = document.getElementById("btn");
-let ll = document.getElementById("btn_1");
-let nav = document.getElementById("nav");
-let title_value = "",
-  content_value = "";
-title.addEventListener("change", () => {
-  let a = title.value;
-  title_value = a;
-});
-content.addEventListener("change", () => {
-  let b = content.value;
-  content_value = b;
-});
+
+let arr = [];
 
 create.addEventListener("click", () => {
-  let div_container = document.createElement("ol");
-  div_container.innerHTML = `
-      <li>${title_value}</li>
-      <li>${content_value}</li>
-   `;
-  div_container.classList.add("list_items");
-  nav.after(div_container);
+   obj = {
+    title_value: title.value,
+    content_value: content.value
+  };
+  arr.push(obj);
+  console.log(arr);
 });
--- Project/Product/index.js
+++
@@ -1,8 +0,0 @@
-let btn = document.getElementById("btn");
-
-btn.addEventListener("click", () => {
-  let num1 = 10;
-  let num2 = parseInt(document.getElementById("num2").value);
-  let heading = document.getElementById("heading");
-  heading.innerText = `two Products Quantity is ${num1 * num2}`;
-});
